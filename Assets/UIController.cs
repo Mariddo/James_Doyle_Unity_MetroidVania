b3e778ce091ba767a7aa7ca2dc80812d849b57eb
mariddo <testname3232@yahoo.com>
2022-09-29 20:18:24 -0500

    Pause Code Complete

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
     public static UIController instance;
     
+    public GameObject pauseScreen;
+
     private void Awake() {
 
         if(instance == null) {
@@ -22,6 +25,8 @@ public class UIController : MonoBehaviour
 
     public TMP_Text text;
 
+    public string mainMenu = "MainMenu";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +38,46 @@ public class UIController : MonoBehaviour
     {
         text.text = Player.instance.currentHitPoints.ToString();
 
+        if(Input.GetKeyDown(KeyCode.Escape)) {
 
+            PauseUnpause();
+        }
+    }
+
+    void PauseUnpause() {
+
+        if (!pauseScreen.activeSelf) {
+
+            pauseScreen.SetActive(true);
+
+            Time.timeScale = 0f;
+        } else {
+
+            pauseScreen.SetActive(false);
+
+            Time.timeScale = 1f;
+        }
+    }
+
+    void MainMenu() {
+
+        SceneManager.LoadScene(mainMenu);
+        Time.timeScale = 1f;
+
+        Destroy(Player.instance.gameObject);
+        Player.instance = null;
+
+        Destroy(RespawnController.instance.gameObject);
+        RespawnController.instance = null;
+    }
+
+    public void Resume() {
+
+        if (pauseScreen.activeSelf) {
+
+            pauseScreen.SetActive(false);
+
+            Time.timeScale = 1f;
+        }
     }
 }
